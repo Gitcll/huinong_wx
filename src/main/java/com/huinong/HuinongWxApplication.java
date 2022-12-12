0260e89a784f575412298c9de8d416c1edf30541
GitCll <302701569qq@gmail.com>
2022-12-12 23:40:24 +0800

    ignore

---
+++ src/main/java/com/huinong/HuinongWxApplication.java
@@ -0,0 +1,13 @@
+package com.huinong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuinongWxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuinongWxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huinong/HuinongWxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huinong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuinongWxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

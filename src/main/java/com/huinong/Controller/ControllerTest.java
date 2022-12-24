11f2499206fdc697c19ced744df1ef70087fd0d0
GitCll <302701569qq@gmail.com>
2022-12-24 15:27:23 +0800

    test接口

--- src/main/java/com/huinong/Controller/ControllerTest.java
+++ src/main/java/com/huinong/Controller/ControllerTest.java
@@ -1,13 +1,24 @@
 package com.huinong.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class ControllerTest {
 
-    @RequestMapping()
+    @GetMapping("index")
     public String test(){
         return "index";
+    }
+
+    @PostMapping("count")
+    public String count(@RequestBody Map<String, Integer> jsonDate) {
+
+        String date = "";
+        Integer num1 = jsonDate.get("num1");
+        Integer num2 = jsonDate.get("num2");
+        date = String.valueOf(num1 + num2);
+        return date;
     }
 }
